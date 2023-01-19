9cc21e1a3e08054782e2440f118c1d9185ccc5bd
Eddie Taliaferro <eddielacrosse2@gmail.com>
2023-01-18 23:29:36 -0500

    Added Source Folder w/ Additional Monopoly_AR Module to Monopoly_AR.uproject file

---
+++ Source/Monopoly_AR/Unreal_init.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Unreal_init.h"
+
+Unreal_init::Unreal_init()
+{
+}
+
+Unreal_init::~Unreal_init()
+{
+}
---
+++ Source/Monopoly_AR/Unreal_init.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MONOPOLY_AR_API Unreal_init
+{
+public:
+	Unreal_init();
+	~Unreal_init();
+};
